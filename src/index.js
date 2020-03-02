0eab0495e4663d673f837a8e8c96bcaf7006536c
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-02 13:19:55 -0700

    Imported and wrapped <App> in Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from "react-redux";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    rootElement
+);
