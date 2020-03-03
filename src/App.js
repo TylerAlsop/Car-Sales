6977c34a321f2dc101725ee679233f6ccba4b640
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-03 13:54:55 -0700

    Imported connect to all the components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStore } from "redux";
 import { rootReducer } from './reducers';
+import { connect } from 'react-redux';
+
 
 
 import Header from './components/Header';
